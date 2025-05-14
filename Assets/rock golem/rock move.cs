0e8e1119b62d5cb5c18d0153b6a1ebb7825db749
Zomborge <darth.rub3ns@gmail.com>
2025-05-14 22:33:26 +1200

    rocks
    rocks

---
+++ Assets/rock golem/rock move.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rockmove : MonoBehaviour
+{
+    public Transform player;
+    [SerializeField]
+    float speed = 2.0f;
+    private void Update()
+    {
+        transform.LookAt(player);
+        Vector3 newPosition = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        transform.position = newPosition;
+    }
+}
