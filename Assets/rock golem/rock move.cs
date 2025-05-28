ddf5d91016d99d54a4b94e548ba367365e8687eb
Zomborge <darth.rub3ns@gmail.com>
2025-05-29 11:21:32 +1200

    roko
    rkoko the crock

--- Assets/rock golem/rock move.cs
+++ Assets/rock golem/rock move.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class rockmove : MonoBehaviour
 {
     private GameObject player;
-    
+
+
     [SerializeField]
+    private Animator _jumpAnimation;
     float speed = 2.0f;
     private void Start()
     {
@@ -18,5 +20,12 @@ public class rockmove : MonoBehaviour
         Vector3 newPosition = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
         transform.position = newPosition;
         transform.Rotate(0f, 90.0f, 0.0f);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "jump it")
+        {
+            _jumpAnimation.SetTrigger("jumo");
+        }
     }
 }
